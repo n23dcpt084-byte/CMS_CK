00c5415d8a2d1e3b66a83e61a44db296cd2b1284
n23dcpt084@student.ptithcm.edu.vn <n23dcpt084@student.ptithcm.edu.vn>
2025-12-15 04:50:27 +0700

    update2

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Res,
   UseGuards,
+  Request,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AuthGuard } from '@nestjs/passport';
